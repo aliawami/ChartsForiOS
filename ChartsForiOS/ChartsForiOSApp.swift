77bd02ebcf93b9e016d5bc1db60ed310f4400709
Ali Alawami <alis2012@gmail.com>
2022-03-07 12:45:15 +0300

    Initial Commit

---
+++ ChartsForiOS/ChartsForiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsForiOSApp.swift
+//  ChartsForiOS
+//
+//  Created by Ali Alawami on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChartsForiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartsForiOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChartsForiOS
+//
+//  Created by Ali Alawami on 07/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
